698e1b3263fe99425e90e5c3929c6e76f1845e23
TARANG MISHRA <mishratarang123@gmail.com>
2025-11-06 21:12:09 +0530

    Update verifyUser.js

--- BACKENED/utilis/verifyUser.js
+++ BACKENED/utilis/verifyUser.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const transporter=nodemailer.createTransport({
 
      host:process.env.EMAIL_HOST ,
-     port:process.env.EMAIL_PORT ,
+     port:Number(process.env.EMAIL_PORT) ,
      secure: true, // true for 465, false for other ports
      auth: {
        user: process.env.EMAIL_USER,
