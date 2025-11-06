f9e1167073a240df815e8ece22d3728badde8efd
TARANG MISHRA <mishratarang123@gmail.com>
2025-11-06 15:15:08 +0530

    Update Dbconnect.js

--- BACKENED/config/Dbconnect.js
+++ BACKENED/config/Dbconnect.js
@@ -5,10 +5,7 @@ const url = process.env.DB_URL;
 
 async function dbconnect() {
   try {
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(url);
     console.log("DB connected");
   } catch (error) {
     console.error("DB connection failed:", error);
