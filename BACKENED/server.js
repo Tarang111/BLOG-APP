8e64130d8b7803a83ed42d2362a2a88f033d80a8
TARANG MISHRA <mishratarang123@gmail.com>
2025-11-06 16:01:48 +0530

    Update server.js

--- BACKENED/server.js
+++ BACKENED/server.js
@@ -10,7 +10,7 @@ require("dotenv").config()
 
 const port=process.env.PORT
 app.use(express.json())
-app.use(cors({origin:process.env.FRONTENED_URL}))
+app.use(cors({origin:"*"}))
 app.get("/",(req,res)=>{
     res.send("hello ji")
 })
