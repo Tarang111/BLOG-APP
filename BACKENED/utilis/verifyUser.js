7e56eb1550b18590570267f1fb7c3749f422526f
TARANG MISHRA <mishratarang123@gmail.com>
2025-11-07 20:17:45 +0530

    Update verifyUser.js

--- BACKENED/utilis/verifyUser.js
+++ BACKENED/utilis/verifyUser.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com", // Brevo SMTP host
-  port: 2525,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.BREVO_SMTP_USER, // SMTP login
